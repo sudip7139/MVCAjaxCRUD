ede10ac06708d367a09bbfd691c6435480d56f3f
sudip chand <sudipchand39@gmail.com>
2020-11-11 11:11:28 +0545

    Add project files.

---
+++ MVCAjaxCRUD/Controllers/EmployeeController.cs
@@ -0,0 +1,40 @@
+﻿using MVCAjaxCRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCAjaxCRUD.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ViewAll()
+        {
+            return View(GetAllEmployee());
+        }
+        [HttpGet]
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddOrEdit()
+        {
+            return View();
+        }
+        IEnumerable<Employee> GetAllEmployee()
+        {
+            using(MVCAjaxEntities db = new MVCAjaxEntities())
+            {
+                return db.Employees.ToList<Employee>();
+            }
+        }
+    }
+}
---
+++ MVCAjaxCRUD/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCAjaxCRUD.MvcApplication" Language="C#" %>
